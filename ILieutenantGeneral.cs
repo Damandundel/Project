53b69c7c3a2920637dc39791c7916cfc16d96573
Damandundel <126072568+Damandundel@users.noreply.github.com>
2025-04-28 10:07:35 +0300

    Delete ILieutenantGeneral.cs

--- ILieutenantGeneral.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MilitaryElite.Interfaces
-{
-    using System.Collections.Generic;
-
-    public interface ILieutenantGeneral
-    {
-        public ICollection<IPrivate> Privates { get; }
-    }
-}
